b4a77156b533dba972b0d1ca0b4a5e846c969a1b
h0tw4t3r <hotwater438@tutanota.com>
2020-04-16 20:02:57 +0300

    frontend: last bugfix lmao, i love jsx comments

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -26,8 +26,8 @@ const Header = ({ classes }) => {
         </Toolbar>
         <Divider />
       </AppBar>
-      /* if the error came from GoLang, it is not a JavaScript error object and doesn`t
-      have message prop. I will open an issue on WebView about that */
+      {/* if the error came from GoLang, it is not a JavaScript error object and doesn`t
+      have message prop. I will open an issue on WebView about that */}
       {searchError && (
         <Alert severity='error'>
           {searchError.message ? searchError.message : searchError}
