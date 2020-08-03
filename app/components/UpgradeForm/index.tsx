6fa7db65d076300f5340bb46b275d4367b68ca04
h0tw4t3r <h0tw4t3r@parrotsec.org>
2020-08-03 15:34:23 +0300

    optimized updateform, don't search for previews

--- app/components/UpgradeForm/index.tsx
+++ app/components/UpgradeForm/index.tsx
@@ -1,20 +1,9 @@
-import {
-  Box,
-  Button,
-  CircularProgress,
-  Grid,
-  List,
-  ListItem,
-  makeStyles,
-  Paper
-} from '@material-ui/core'
+import { Button, CircularProgress, Grid, Link, makeStyles, Paper } from '@material-ui/core'
 import { unwrapResult } from '@reduxjs/toolkit'
 import React, { useEffect, useState } from 'react'
 import { AlertActions, AptActions, QueueActions } from '../../actions'
 import { connect, ConnectedProps } from 'react-redux'
 import { CheckCircleOutline as SuccessIcon } from '@material-ui/icons'
-import PackagePreview from '../PackagePreview'
-import { Preview } from '../../actions/apt'
 import { push } from 'connected-react-router'
 import { withRouter } from 'react-router'
 import { RouteComponentProps } from 'react-router-dom'
@@ -35,7 +24,6 @@ const mapStateToProps = ({ queue: { packages } }: RootState) => ({
 const mapDispatchToProps = {
   upgrade: QueueActions.upgrade,
   checkUpdates: AptActions.checkUpdates,
-  searchPreviews: AptActions.searchPreviews,
   setAlert: AlertActions.set,
   push
 }
@@ -44,28 +32,17 @@ const connector = connect(mapStateToProps, mapDispatchToProps)
 
 type UpgradeFormProps = ConnectedProps<typeof connector> & RouteComponentProps
 
-const UpgradeForm = ({
-  checkUpdates,
-  searchPreviews,
-  upgrade,
-  push,
-  packages
-}: UpgradeFormProps) => {
+const UpgradeForm = ({ checkUpdates, upgrade, push, packages }: UpgradeFormProps) => {
   const classes = useStyles()
   const [loading, setLoading] = useState(true)
-  const [previews, setPreviews] = useState(Array<Preview>())
-
-  const [showRest, setShowRest] = useState(false)
+  const [updates, setUpdates] = useState(Array<string>())
 
   useEffect(() => {
     const f = async () => {
       try {
-        const updates = unwrapResult(await checkUpdates())
-        setPreviews(
-          await Promise.all(updates.map(async name => unwrapResult(await searchPreviews(name))[0]))
-        )
+        setUpdates(unwrapResult(await checkUpdates()))
       } catch (e) {
-        setPreviews([])
+        setUpdates([])
       }
       setLoading(false)
     }
@@ -85,59 +62,44 @@ const UpgradeForm = ({
         <>
           <Grid item xs>
             <h2>
-              {previews.length !== 0
-                ? `${previews.length} updates available! Upgrade now!`
+              {updates.length !== 0
+                ? `${updates.length} updates available! Upgrade now!`
                 : 'Your system is up to date'}
             </h2>
           </Grid>
-          {previews.length !== 0 ? (
+          {updates.length !== 0 ? (
             <>
-              <Paper elevation={10}>
-                <List style={{ maxHeight: 600, overflow: 'auto' }}>
-                  {previews
-                    .slice(0, 3)
-                    .filter(({ name }) =>
-                      packages.every(({ name: packageName }) => packageName !== name)
+              <Paper style={{ padding: '1rem' }} elevation={10}>
+                {updates
+                  .filter(name => packages.every(({ name: packageName }) => packageName !== name))
+                  .map((upgradableName, i, dependsSplitted) => {
+                    return (
+                      <>
+                        <Link
+                          component="button"
+                          key={`${name}-upgradable-link-${upgradableName}`}
+                          variant="body1"
+                          onClick={() => push(`/package/${upgradableName}`)}
+                        >
+                          {upgradableName}
+                        </Link>
+                        {i !== dependsSplitted.length - 1 && ', '}
+                      </>
                     )
-                    .map(({ name, description }) => (
-                      <ListItem key={name}>
-                        <PackagePreview name={name} description={description} />
-                      </ListItem>
-                    ))}
-                  {showRest &&
-                    previews
-                      .slice(3)
-                      .filter(({ name }) =>
-                        packages.every(({ name: packageName }) => packageName !== name)
-                      )
-                      .map(({ name, description }) => (
-                        <ListItem key={name}>
-                          <PackagePreview name={name} description={description} />
-                        </ListItem>
-                      ))}
-                </List>
+                  })}
               </Paper>
-              <Box display="flex" marginTop={3}>
-                <Button
-                  size="large"
-                  variant="contained"
-                  onClick={() => setShowRest(!showRest)}
-                  style={{ marginRight: '1rem' }}
-                >
-                  {showRest ? 'Minimize' : 'Expand'}
-                </Button>
-                <Button
-                  size="large"
-                  variant="contained"
-                  color="primary"
-                  onClick={() => {
-                    previews.map(({ name }) => upgrade(name))
-                    push('/queue')
-                  }}
-                >
-                  Upgrade
-                </Button>
-              </Box>
+              <Button
+                size="large"
+                variant="contained"
+                color="primary"
+                style={{ marginTop: '1rem' }}
+                onClick={() => {
+                  updates.map(name => upgrade(name))
+                  push('/queue')
+                }}
+              >
+                Upgrade
+              </Button>
             </>
           ) : (
             <SuccessIcon color="primary" fontSize="large" />
