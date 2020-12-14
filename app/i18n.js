3e6f1726a1daac28e7e85a26490661679b617310
Dario <598-danterolle@users.noreply.nest.parrot.sh>
2020-12-14 10:04:21 +0000

    Added browser language detector

--- app/i18n.js
+++ app/i18n.js
@@ -1,17 +1,20 @@
 import i18next from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import HttpApi from "i18next-http-backend"
+import LanguageDetector from "i18next-browser-languagedetector";
 
 i18next
 .use(initReactI18next)
 .use(HttpApi)
+.use(LanguageDetector)
 .init({
     debug: true,
     defaultNS: "translation",
+    ns: ["translation"],
     preload: ["en", "it"],
-    lng: "en",
     fallbackLng: "en",
     supportedLngs: ["en", "it"],
+    nonExplicitSupportedLngs: true,
     backend: {
         loadPath: './locales/{{lng}}/{{ns}}.json',
         allowMultiLoading: false,
